cdb084ecb96544dbd71515572fb011a8a46dcf1f
Manuel Swarzstrunzer <manuel@manuel.com>
2024-04-16 14:59:41 +0200

    fix path import Cart component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
-import Cart from './components/Cart';
+import Cart from './components/Cart/Cart';
 
 function App() {
   return (
